0c55c06051c2f6be6bcbbe525e14b8f6d4bfe558
Juan Davila <juandavil1101@gmail.com>
2021-07-23 21:00:40 -0500

    Add project files.

---
+++ be_UrbanPlanner/Building.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace be_UrbanPlanner
+{
+  class Building
+  {
+    public int Stories { get; set; }
+    public double Width { get; set; }
+    public double Depth { get; set; } 
+    public double Volume { get; private set; }
+
+    string _designer; // will hold your name
+    DateTime _dateConstructed; // when was the building created
+    string _address; 
+    string _owner;  // this will store the name of the person that owns the building
+
+    public Building(string address)
+    {
+      _designer = "onlyJuan";
+      _address = address;
+      _dateConstructed = DateTime.Now;
+    }
+
+    public void Purchase(string owner)
+    {
+      _owner = owner;
+    }
+
+  public void SetValue()
+    {
+      Volume = Width * Depth * (3 * Stories);
+    }
+
+    public void PrintBuilding()
+    {
+      Console.WriteLine(String.IsNullOrWhiteSpace(_address) ? "" : $"\n\nBuilding address; {_address}");
+      Console.WriteLine(String.IsNullOrWhiteSpace(_designer) ? "" : $"Designed by; {_designer}");
+      Console.WriteLine($"Constructed on ; {_dateConstructed}");
+      Console.WriteLine(String.IsNullOrWhiteSpace(_owner) ? "" : $"Owner by: {_owner}");
+      Console.WriteLine($"{Volume} cubic meters of space");
+    }
+
+
+  }
+}
---
+++ be_UrbanPlanner/UrbanPlanner.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace be_UrbanPlanner
+{
+  class UrbanPlanner
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Urban Planner");
+      var buildingJuan = new Building("1428 Elm Street Springwood, Ohio");
+      var buildingTwo = new Building("124 Conch Street Bikini Bottom");
+      var buildingThree = new Building("58N 018, 439 East District");
+      var buildingFour = new Building("283 Blvd De France, Parris Island, SC 29905");
+
+    
+
+      buildingJuan.Stories = 10;
+      buildingJuan.Width = 3;
+      buildingJuan.Depth = 2;
+      buildingJuan.SetValue();
+      buildingJuan.Purchase("Freddy Krouger");
+      buildingJuan.PrintBuilding();
+
+      buildingTwo.Stories = 11;
+      buildingTwo.Width = 2;
+      buildingTwo.Depth = 2;
+      buildingTwo.SetValue();
+      buildingTwo.Purchase("Sponge Bob");
+      buildingTwo.PrintBuilding();
+
+      buildingThree.Stories = 10000;
+      buildingThree.Width = 10;
+      buildingThree.Depth = 1000;
+      buildingThree.SetValue();
+      buildingThree.Purchase("Goku");
+      buildingThree.PrintBuilding();
+
+      buildingFour.Stories = 1775;
+      buildingFour.Width = 1;
+      buildingFour.Depth = 1;
+      buildingFour.SetValue();
+      buildingFour.Purchase("Chesty Pollard");
+      buildingFour.PrintBuilding();
+
+
+
+
+
+
+
+    }
+  }
+}
